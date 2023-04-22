50388a4b116fb5730ecfa15335ab533bc22e6e66
smbirch <spencer.m.birch@gmail.com>
2023-04-22 12:59:40 -0700

    creating RSVP and more info pages setting up routes and basic contact form

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from forms import ContactForm
 
 app = Flask(__name__)
+app.secret_key = "development key"
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/rsvp", methods=["GET", "POST"])
+def rsvp():
+    form = ContactForm()
+
+    if request.method == "POST":
+        return "form posted"
+    return render_template("rsvp.html", form=form)
+
+
+@app.route("/info")
+def info():
+    return render_template("info.html")
