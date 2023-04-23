87e97d680bf96b168859a6509fdc4bace7d9582d
smbirch <spencer.m.birch@gmail.com>
2023-04-22 21:57:53 -0700

    rsvp form now works

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template, request
+import config
+
+from flask import Flask, render_template, request, flash
 from forms import ContactForm
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
-app.secret_key = "development key"
+app.secret_key = config.app_secret_key
+
+
+mail = Mail()
+app.config["MAIL_SERVER"] = "smtp.office365.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = "birchwedding@outlook.com"
+app.config["MAIL_PASSWORD"] = config.email_password
+mail.init_app(app)
 
 
 @app.route("/")
@@ -13,10 +26,25 @@ def index():
 @app.route("/rsvp", methods=["GET", "POST"])
 def rsvp():
     form = ContactForm()
-
     if request.method == "POST":
-        return "form posted"
-    return render_template("rsvp.html", form=form)
+        if form.validate() == False:
+            flash("All fields are required")
+            return render_template("rsvp.html", form=form)
+        else:
+            msg = Message(
+                form.subject.data,
+                sender="birchwedding@outlook.com",
+                recipients=["birchwedding@outlook.com"],
+            )
+            msg.body = """From: %s <%s>%s""" % (
+                form.name.data,
+                form.email.data,
+                form.message.data,
+            )
+            mail.send(msg)
+            return render_template("rsvp.html", success=True)
+    elif request.method == "GET":
+        return render_template("rsvp.html", form=form)
 
 
 @app.route("/info")
--- forms.py
+++ forms.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
-
 from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 
 class ContactForm(FlaskForm):
-    name = StringField("Name")
+    name = StringField("Name", validators=[DataRequired()])
 
-    email = StringField("Email")
+    email = StringField("Email", validators=[DataRequired(), Email()])
 
-    message = TextAreaField("Message")
+    subject = StringField("Subject", validators=[DataRequired()])
 
-    submit = SubmitField("Send")
+    message = TextAreaField("Message", validators=[DataRequired()])
+
+    submit = SubmitField("Submit")
