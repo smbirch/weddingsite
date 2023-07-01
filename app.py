557149ef5f416c0ad191ac500bc2d380978afa9e
smbirch <spencer.m.birch@gmail.com>
2023-06-30 22:05:00 -0700

    fixing secret key

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ load_dotenv()
 
 
 app = Flask(__name__)
-# app.secret_key = os.getenv("app_secret_key")
-app.secret_key = "secret_key"
+app.secret_key = os.getenv("app_secret_key")
 
 
 mail = Mail()
